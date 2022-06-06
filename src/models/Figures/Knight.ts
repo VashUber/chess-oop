c221ae714b7208c78cb1b258762d78bc93d5a7a5
VashUber <mrjarokvk@gmail.com>
2022-06-06 11:24:00 +0300

    feature: added moves for knight

--- src/models/Figures/Knight.ts
+++ src/models/Figures/Knight.ts
@@ -1,10 +1,30 @@
 import { figure_color, coords, Names } from "../../types";
+import Cell from "../Cell";
 import Figure from "../Figure";
 
 class Knight extends Figure {
   constructor(color: figure_color, coords: coords) {
     super(color, coords);
     this.name = Names.KNIGHT;
+  }
+
+  makeMove(target: Cell) {
+    this.coords = target.coords;
+    target.figure = this;
+  }
+
+  canMoveOn(target: Cell) {
+    const xd = Math.abs(this.coords[0] - target.coords[0]);
+    const yd = Math.abs(this.coords[1] - target.coords[1]);
+
+    if (
+      ((xd === 1 && yd === 2) || (xd === 2 && yd === 1)) &&
+      target.figure?.color !== this.color
+    ) {
+      return true;
+    }
+
+    return false;
   }
 }
 
